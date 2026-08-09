c000f15d7365b18f82c99d4958100739c2613fa1
karthik <karthidasari5346@gmail.com>
2026-08-09 21:19:04 +0530

    Disable ESLint warnings for GestureDetector

--- frontend/src/components/GestureDetector.jsx
+++ frontend/src/components/GestureDetector.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState, useCallback } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { Hands, HAND_CONNECTIONS } from '@mediapipe/hands';
 import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
 import { Camera } from '@mediapipe/camera_utils';
